0aeb10471c60862c5a21944024a63f618a8cec1d
velmurugan08 <vel836403@gmail.com>
2024-04-02 10:44:13 +0530

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys
-import logging
+'''from logger import logging'''
+
+
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -16,8 +18,8 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
+ 
     
-
 '''if __name__=="__main__":
     try:
         a=1/0
